01eb4144c9f46396dd74dc745a17b989ed5f9402
Adam Domachowski <adam.domachowskig6@gmail.com>
2022-11-07 23:45:16 +0100

    Visualisation improves

--- ObliqueThrow.py
+++ ObliqueThrow.py
@@ -1,16 +1,20 @@
 # Creating Space for program, basics parameters etc.
 from cmath import pi
+from datetime import time
 from time import sleep
 import turtle
 import math
 
-
-# Najpierw zapisujemy tylko Y za X odpowiednie beda inne funckje finalnie wszystko sklejamy w jedno
+TIMESTEP = 0.5 # Necesary to simulation
 
 # Class
 class object:
-    listX = []
-    listY = []
+
+    list_corx = []
+    list_cory = []
+    list_velx = []
+    list_vely = []
+
     def __init__(self,velocity,acceleration,timeofengine,startangle):
         self.velocity = velocity
         self.acceleration = acceleration
@@ -19,39 +23,53 @@ class object:
         self.corx = 0
         self.cory = 0
 
+
+
     # Engine Force 
     def thrust(self):
         time = 0
         while time < self.timeofengine:
-            time += 0.1
-            X = self.corx + math.cos(self.startangle)*(self.velocity*time + 0.5*self.acceleration*time**2)
-            Y = self.cory + math.sin(self.startangle)*(self.velocity*time + 0.5*self.acceleration*time**2)
-            self.listX.append(X)
-            self.listY.append(Y)
-        print("engine off")
+            time += TIMESTEP
+            S = (self.velocity*time + 0.5*self.acceleration*time**2)
+            V = (self.velocity +self.acceleration*time)
+            x = self.corx + math.cos(self.startangle)*S
+            y = self.cory + math.sin(self.startangle)*S
+            VEL_X = math.cos(self.startangle)*V
+            VEL_Y = math.sin(self.startangle)*V
+            self.list_corx.append(x)
+            self.list_cory.append(y)
+            self.list_velx.append(VEL_X)
+            self.list_vely.append(VEL_Y)
+
+
 
     # Freefall
     def freefall(self):
         time = 0
-        corx2 = self.corx + math.cos(self.startangle)*(self.velocity*self.timeofengine + 0.5*self.acceleration*self.timeofengine**2)
-        cory2 = self.cory + math.sin(self.startangle)*(self.velocity*self.timeofengine + 0.5*self.acceleration*self.timeofengine**2)
-        velocity2 = self.velocity + self.acceleration*self.timeofengine
-        X = corx2
-        Y = cory2
-        while Y > self.cory: #while rocket hits the ground
-            time += 0.1
-            X = corx2 + math.cos(self.startangle)*velocity2*time
-            Y = cory2 + math.sin(self.startangle)*velocity2*time + 0.5*(-9.81)*time**2
-            self.listX.append(X)
-            self.listY.append(Y)
-     
-    # Printing Coordinates to file
-    def savecoordinates(self):
-        file = open("coordinates.txt","w")
+        S = self.velocity*self.timeofengine + 0.5*self.acceleration*self.timeofengine**2 # distance so far
+        V = self.velocity + self.acceleration*self.timeofengine # velocity reach while engine of
+        corx2 = self.corx + math.cos(self.startangle)*S # calculating coordinates where engine off
+        cory2 = self.cory + math.sin(self.startangle)*S 
+        x = corx2   # set this coordinates as a new start point (from here we uses other formula)
+        y = cory2
+        while y > self.cory: #while rocket hits the ground
+            time += TIMESTEP      
+            x = corx2 + (math.cos(self.startangle)*V*time) 
+            y = cory2 + (math.sin(self.startangle)*V*time + 0.5*(-9.81)*time**2)
+            VEL_X = math.cos(self.startangle)*V
+            VEL_Y = math.sin(self.startangle)*V + (-9.81)*time
+            self.list_corx.append(x)
+            self.list_cory.append(y)
+            self.list_velx.append(VEL_X)
+            self.list_vely.append(VEL_Y)   
 
+    # Printing Coordinates to file
+    def save_datas_to_file(self):
+        file = open("datas.txt","w")
+        file.write("TIME COR_X COR_Y VEL_X VEL_Y ACCELERATION\n")
         i = 0
-        while i < len(self.listY):
-            file.write(str(round(self.listX[i],4)) + " " + str(round(self.listY[i],4)) + "\n")
+        while i < len(self.list_cory):
+            file.write( str(i+1) + " " + str(round(self.list_corx[i],4)) + " " + str(round(self.list_cory[i],4)) + " " + str(round(self.list_velx[i],4)) + " " + str(round(self.list_vely[i],4)) + "\n")
             i += 1
         file.close()
 
@@ -59,13 +77,13 @@ class object:
     def simulate(self):
 
         # Depending On X and Y sizes program will adjust visual simulation to fit trajectory in window
-        if len(self.listX)>0:
-            scale_x = 400/max(self.listX)
+        if len(self.list_corx)>0:
+            scale_x = 400/max(self.list_corx)
         else:
             scale_x = 0
 
-        if len(self.listY)>0:
-            scale_y = 400/max(self.listY)
+        if len(self.list_cory)>0:
+            scale_y = 400/max(self.list_cory)
         else:
             scale_y = 0
 
@@ -82,7 +100,7 @@ class object:
         wn.setup(width=800, height=800)
         wn.tracer(0)
 
-        # Surface (define as X = -300)
+        # Surface
         surface_line = turtle.Turtle()
         surface_line.speed(0)
         surface_line.shape("square")
@@ -100,32 +118,29 @@ class object:
         point.penup()
         point.goto(self.corx,self.cory)
 
-        #Pixel - used for draw trajectory
-
 
         # Simulation Process
         i = 0
-        while i < len(self.listX):
+        while i < len(self.list_corx):
             point.color("red")
             point.shapesize(stretch_wid=0.5, stretch_len=0.5)
             wn.update()
-            point.setx((self.listX[i])*scale - 300)
-            point.sety((self.listY[i])*scale - 300)
+            point.setx((self.list_corx[i])*scale - 300)
+            point.sety((self.list_cory[i])*scale - 300)
             point.shapesize(stretch_wid=0.1, stretch_len=0.1)
             point.color("blue")
             point.stamp()
             
-            #print((str)((self.listX[i])*scale - 300) + " " + (str)((self.listY[i])*scale - 300))
+            #print((str)((self.list_corx[i])*scale - 300) + " " + (str)((self.list_cory[i])*scale - 300))
             i += 1
             sleep(0.001)
 
 
-
 # Main
-rocket = object(0,10,20,pi/4)
+rocket = object(0,9.81,100,pi/2.33)
 rocket.thrust()
 rocket.freefall()
-rocket.savecoordinates()
+rocket.save_datas_to_file()
 rocket.simulate()
 sleep(2)
 
